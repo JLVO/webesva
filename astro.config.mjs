7855281c429c826803dfe755c60ea94785d5978b
Luis Vargas <j.luisvargas1990@gmail.com>
2025-08-29 14:10:21 -0500

    fix: Configure Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import mdx from '@astrojs/mdx'; // Importa la integración MDX
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
+import vercel from '@astrojs/vercel';
 
 import sharp from 'astro/assets/services/sharp'; // Importación directa del módulo
 export default defineConfig({
-  integrations: [tailwind(), react(),sitemap(),mdx()],
+  output: 'server',
+  integrations: [tailwind(), react(),sitemap(),mdx(), vercel()],
   site: "https://esva.pe", // MUY IMPORTANTE: Usa tu dominio aquí
 
   image: {
